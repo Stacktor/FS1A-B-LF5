12dfc80cca4042c9c19f4a23ff8400604f623ec8
Stacktor <78821393+Stacktor@users.noreply.github.com>
2022-11-09 20:27:33 +0100

    Spritpreis

--- Verzweigung 1.py
+++ Verzweigung 1.py
@@ -4,15 +4,10 @@ import os
 
 
 
-
+"""
 menu = tk.Tk()
 menu.title("Projekte")
 menu.geometry("400x400")
-
-# Create a Tkinter variable
-tkvar = tk.StringVar(menu)
-
-
 
 
 
@@ -41,11 +36,27 @@ Temp1 = float(input("Enter the first number: "))
 if Temp1 > 30:
     print("Heute ist ein Schöner Tag")
 
-
+"""
 #Spritpreis
-'''
-Spritpreis = float(input("Bitte geben Sie den Spritpreis ein: "))
-Spritmenge = float(input("Bitte geben Sie die Spritmenge ein: "))
-Spritbetrag = round((Spritpreis * Spritmenge),2)
-print("Spritbetrag: ",Spritbetrag)
-'''
+
+Spritgetankt = float(input("Wie viel Liter Sprit haben Sie getankt? "))
+Spritart = input("Bitte geben Sie die Spritart ein: ")
+
+Diesel = 1.28
+Super = 1.36
+
+Netto = (Diesel * Spritgetankt)
+Brutto = round((Diesel * Spritgetankt) * 0.19 ,2)
+ins = (round((Diesel * Spritgetankt) + Brutto,2))
+
+if Spritart == "Diesel":
+  print(Brutto)
+  print(Netto)
+  print(ins)
+
+elif Spritart == "Super":
+    print(Super)
+
+
+else:
+    print("Falsche Eingabe")
