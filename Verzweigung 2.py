de88f9220697af8a8e8c340e9b8e20a0fc8d3bf1
Stacktor <78821393+Stacktor@users.noreply.github.com>
2022-11-09 21:08:10 +0100

    code2

--- Verzweigung 2.py
+++ Verzweigung 2.py
@@ -1,10 +1,22 @@
-
 #code 1
+
+
 eingabe = float(input("zahl zwischen 10 und 18: "))
+
 if 10 < eingabe < 18:
     print("richtig die zahl ist zwischen 10 und 18")
 else:
     print("falsch")
 
+#code 2
 
-d
+eingabe = float(input("zahl zwischen 2 und 10 oder 20 und 28: "))
+if 2 < eingabe < 10 or 20 < eingabe < 28:
+    print("richtig die zahl ist zwischen 2 und 10 oder 20 und 28")
+else:
+    print("falsch")
+
+#code 3
+
+
+
