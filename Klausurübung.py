4dc938aaec38a3622af190ce758351246c5a7eb5
locitolp <locitolp@icloud.com>
2022-12-05 08:27:06 +0100

    Repariert oder so

--- Klausurübung.py
+++ Klausurübung.py
@@ -3,10 +3,9 @@ monatsgehalt = float(input("Geben Sie Ihr Monatsgehalt in Euro ein: "))
 kinder = float(input("Geben Sie die Anzahl ihrer Kinder ein: "))
 dauerbetrieb = float(input("Arbeitsangehörigkeit im Betrieb in Jahren: "))
 
-# Berechnung
 if dauerbetrieb < 10:
-    fehltage = float(input("Geben Sie die Anzahl der fehlenden Tage ein: "))
-
+    fehltage = float(input("Geben sie fehltage an"))
+# Berechnung
 if dauerbetrieb > 3:
     if dauerbetrieb < 10:
         if fehltage > 20:
