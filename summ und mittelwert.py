db2b7b9368d1ab2499cc3bc047507b1a8db158f0
locitolp <locitolp@icloud.com>
2023-01-25 14:09:31 +0100

    commit

---
+++ summ und mittelwert.py
@@ -0,0 +1,6 @@
+Zahl1=float(input("erste zahl eingeben mit maximal 2 Nachkomma stellen: "))
+Zahl2=float(input("Zweite zahl eingeben mit maximal 2 Nachkomma stellen: "))
+zahl3 = Zahl1+Zahl2
+Zahl4 = zahl3/2
+print("Summe: ", zahl3)
+print("Mittelwert: ",Zahl4)
