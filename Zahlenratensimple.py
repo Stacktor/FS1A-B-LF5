20e64cffedfc280af598c85e03853e7c0cad86e2
locitolp <locitolp@icloud.com>
2023-02-15 14:25:20 +0100

    änderung verbesserung

--- Zahlenratensimple.py
+++ Zahlenratensimple.py
@@ -1,11 +1,16 @@
 from random import*
 Gewinnzahl=randint(1 ,20)
-Spiel = False
-while(Spiel==False):
-    Auswahlzahl=int(input("Raten Sie eine Zahl [1-20]: "))
-    if (Auswahlzahl==Gewinnzahl):
-        Spiel=True
-    else:
-        print ("Leider falsch!")
-print ("Sie haben die Zahl erraten!")
+wiederhol=True
+anykey=True
+while(wiederhol==True):
+    Spiel = False
+    while(Spiel==False):
+        Auswahlzahl=int(input("Raten Sie eine Zahl [1-20]: "))
+        if (Auswahlzahl==Gewinnzahl):
+            Spiel=True
+            print ("Sie haben die Zahl erraten!")
+        else:
+            print ("Leider falsch!")
+    if (input("press Enter to continue")):
+        wiederhol=True
 
