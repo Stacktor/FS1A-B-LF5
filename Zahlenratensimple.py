1cb255f4012c7aebfea48793298052d52a7b9a47
locitolp <locitolp@icloud.com>
2023-02-16 14:00:36 +0100

    lo

--- Zahlenratensimple.py
+++ Zahlenratensimple.py
@@ -1,7 +1,8 @@
 from random import*
 wiederhol=True
 anykey=True
-while(wiederhol==True):
+while(wiederhol==True):  
+    counter=0
     Spiel = False
     Gewinnzahl=randint(1 ,20)
     while(Spiel==False):
@@ -9,11 +10,14 @@ while(wiederhol==True):
         if (Auswahlzahl==Gewinnzahl):
             Spiel=True
             print ("Sie haben die Zahl erraten!")
+            print ("anzahl der Versuche: ",counter)
         else:
             print ("Leider falsch!")
             if(Gewinnzahl>Auswahlzahl):
                 print("ihre zahl ist zu klein")
             else:
                 print("ihre zahl ist zu gross")
+        counter=counter+1
+        print("try: ", counter)
     if (input("press Enter to continue")):
         wiederhol=True        
