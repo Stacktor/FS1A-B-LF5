e67945d26983d747dd1fc47c279b2caa180f8739
Thomas Graf <78821393+Stacktor@users.noreply.github.com>
2023-02-16 14:25:49 +0100

    navigation in einem koordinatensystem Aufgabe 2.py Hinzugefügt

--- navigation in einem koordinatensystem Aufgabe 1.py
+++ navigation in einem koordinatensystem Aufgabe 1.py
@@ -1,23 +1,25 @@
-# Definieren Sie eine Funktion, die die Benutzereingaben (‚o’=Osten, ‚n’=Norden, ‚w’=Westen, ‚s’=Süden) in eine Positionsangabe ( x | y ) umwandelt
-def move_char_by_input(chars, char, input):
-    # Konvertieren Sie die Zeichenfolge in eine Liste
-    char_list = list(chars)
-    # Holen Sie den aktuellen Index des Zielzeichens
-    old_index = char_list.index(char)
-    # Entfernen Sie das Zielzeichen aus der Zeichenliste
-    char = char_list.pop(old_index)
-    # Berechnen Sie den neuen Index basierend auf der Benutzereingabe
-    if input == 'o': # Osten
-        new_index = old_index + 1
-    elif input == 'n': # Norden
-        new_index = old_index - len(char_list) // 2
-    elif input == 'w': # Westen
-        new_index = old_index - 1
-    elif input == 's': # Süden
-        new_index = old_index + len(char_list) // 2
-    else: # Ungültige Eingabe oder Programm beenden ('b')
-        return None
-        # Fügen Sie das Zielzeichen an der neuen Position ein
-    char_list.insert(new_index, char)
-    # Konvertieren Sie die Zeichenliste zurück in eine Zeichenfolge und geben Sie sie zurück
-    return ''.join(char_list)
+# Startpunkt ist x=10, y=10
+x = 10
+y = 10
+
+# Eine Schleife, die die Benutzereingaben liest und verarbeitet
+while True:
+    # Die aktuelle Position ausgeben
+    print(f"Sie sind bei {x} | {y}.")
+
+    # Die Benutzereingabe lesen
+    eingabe = input("Wohin wollen Sie gehen? (o/n/w/s/b): ")
+
+    # Die Eingabe überprüfen und die Position entsprechend ändern
+    if eingabe == "o":
+        x += 1 # Nach Osten gehen bedeutet x um 1 erhöhen
+    elif eingabe == "n":
+        y += 1 # Nach Norden gehen bedeutet y um 1 erhöhen
+    elif eingabe == "w":
+        x -= 1 # Nach Westen gehen bedeutet x um 1 verringern
+    elif eingabe == "s":
+        y -= 1 # Nach Süden gehen bedeutet y um 1 verringern
+    elif eingabe == "b":
+        break # Das Programm beenden
+    else:
+        print("Ungültige Eingabe. Bitte geben Sie o, n, w, s oder b ein.")
