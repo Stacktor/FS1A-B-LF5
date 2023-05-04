10defd97d7302f78478d9b268cdf1c645eb5e7f5
locitolp <locitolp@icloud.com>
2023-05-04 13:30:31 +0200

    better

--- BMI Rechner.py
+++ BMI Rechner.py
@@ -1,32 +1,33 @@
-h=float(input("gebe höhe in cm an: "))
+hcm=float(input("gebe höhe in cm an: "))
+hm = hcm*0.01
 w=float(input("gebe gewicht in Kg an: "))
 geschlecht=(input("geben sie ihr geschlecht an (weiblich/mänlich)")) 
-BMI=w/(h*h)
+BMI=w/(hm*hm)
 print("Der berechnete BMI liegt bei:  ",BMI)
-print("ihr normalgewicht liegt bei:  ",h-100)
-print("dein idealgewicht wäre bei:  ",(h-100)*(-0.1)+(h-100)) 
+print("ihr normalgewicht liegt bei:  ",hcm-100)
+print("dein idealgewicht wäre bei:  ",(hcm-100)*(-0.1)+(hcm-100)) 
 if(BMI>0):
     if(geschlecht == "weiblich" or "Weiblich"):
         if(BMI<19):
             print("du bist untergewichtig")
-        elif(BMI>=19):
+        elif(BMI<=19):
             print("du bist normalgewichtig")
-        elif(BMI>=25):
+        elif(BMI<=25):
             print("du bist übergewichtig")
-        elif(BMI>=40):
+        elif(BMI<=40):
             print("du bist adipös")
         else: 
             print("du bist stark adipös")
     else:
-        if(BMI<=16):
+        if(BMI<=20):
             print("du bist sehr untergewichtig")
-        elif(BMI<=18.5):
-            print("du bist leicht untergewichtig")
         elif(BMI<=25):
-            print("kratuliere du hast ein gesundes gewicht")
+            print("du bist normalgewichtig")
         elif(BMI<=30):
-            print("du bist leicht übergewichtig")
+            print("du bist übergewichtig")
+        elif(BMI<=40):
+            print("du bist adipös")
         else: 
-            print("du bist sehr übergewichtig")
+            print("du bist stark adipös")
 else:
     print("gebe richtige werte an")
