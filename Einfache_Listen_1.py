42b261bc5d8bcde6a46dc7c8cf7b87016420fdab
Thomas Graf <78821393+Stacktor@users.noreply.github.com>
2023-06-14 14:34:34 +0200

    File dings

--- Einfache_Listen_1.py
+++ Einfache_Listen_1.py
@@ -1,3 +1,27 @@
-Tage = [Montag, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag]
+Tage = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
 
-print  Tage(2)
+print ("A")
+print  (Tage[0])
+
+print ("B")
+print (Tage[6])
+
+print("C")
+for i in range (0,7,1):
+    print (Tage[i])
+
+print("D)")
+for i in range (7,0,-1):
+    print (Tage[i-1])
+
+
+x = len (Tage)
+print(x)
+
+Tage.pop (1)
+Tage.pop (5)
+print (Tage)
+
+
+
+
